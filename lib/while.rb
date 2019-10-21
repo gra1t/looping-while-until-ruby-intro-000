3dac7aecaf69b21c6914c4f978aeed9870139eeb
granit <graniitkastrati@gmail.com>
2019-10-21 13:56:29 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,9 @@
 def using_while
   levitation_force = 6
-	
+
   #your code here
-  levitation_force += 1
+  while levitation_force >= 1
+  levitation_force -= 1
   puts "Wingardium Leviosa"
-  
+
 end
-
-
